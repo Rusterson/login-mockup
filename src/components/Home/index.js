8eb54d7c0b990138ae3fa6b88f30dc6a44128d24
rusterson <rusterson_sato@yahoo.com>
2019-08-13 13:03:11 -0300

    logo fixed

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import styles from "./home.module.css";
-import { ReactComponent as Logo } from "../../assets/logo/logo.svg";
+import logo from "../../assets/logo/logo.svg";
 import { Link } from 'react-router-dom'
 
 function Home() {
@@ -8,7 +8,7 @@ function Home() {
     <div className={styles.wrapper}>
       <div className={styles.box}>
         <h2 className={styles.wellcome}>Wellcome</h2>
-        <Logo className={styles.logo} />
+        <img src={logo} alt="" className={styles.logo} />
         <h1 className={styles.title}>Brain</h1>
         <span className={styles.links}>
           <Link to='/login'>Login</Link> or <Link to='/register'> Register</Link>
